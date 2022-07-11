a20eb0e3e37709f1e3ef1f927ad9cce464da6ad7
iamtheblackunicorn <officialrealalexanderabraham@gmail.com>
2022-07-11 17:45:36 +0200

    Revisison 1 done.

--- src/components/ModelCog.vue
+++ src/components/ModelCog.vue
@@ -10,7 +10,11 @@ element that holds our actual
 3D model.
 -->
 <template>
- <canvas></canvas>
+<!--
+Nothing goes here
+because we insert the element
+in the script section.
+-->
 </template>
 
 <!--
@@ -24,7 +28,7 @@ the model for different screen sizes.
 import * as THREE from 'three';
 
 // The URL to the matcap URL.
-const textureUrl = 'https://blckunicorn.art/assets/matcaps/01.png';
+const textureUrl = 'https://blckunicorn.art/assets/matcaps/1.png';
 
 // The function to display the model
 // for desktops.
@@ -33,6 +37,7 @@ function desktopModel(){
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight);
     camera.position.z = 3;
     camera.position.x = -1;
+    camera.position.y = 0.3;
     scene.add(camera);
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load(textureUrl);
@@ -42,6 +47,8 @@ function desktopModel(){
     mesh.rotation.y = 0.5;
     scene.add(mesh);
     const renderer = new THREE.WebGLRenderer({alpha:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.append(renderer.domElement);
     const cursor = {x:0, y:0};
     const tick = () => {
         window.requestAnimationFrame(tick);
@@ -55,7 +62,30 @@ function desktopModel(){
 // The function to display the model
 // for other screen sizes.
 function responsiveModel(){
-
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight);
+    camera.position.z = 4;
+    camera.position.x = 0.17;
+    camera.position.y = 0.6;
+    scene.add(camera);
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load(textureUrl);
+    const geometry = new THREE.TorusKnotGeometry(0.5,0.2);
+    const material = new THREE.MeshMatcapMaterial({matcap: texture});
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.y = 0.5;
+    scene.add(mesh);
+    const renderer = new THREE.WebGLRenderer({alpha:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.append(renderer.domElement);
+    const cursor = {x:0, y:0};
+    const tick = () => {
+        window.requestAnimationFrame(tick);
+        mesh.rotation.y += 0.01;
+        mesh.rotation.x += 0.01;
+        renderer.render(scene, camera);
+    }
+    tick();
 }
 
 // Calling the respective functions
